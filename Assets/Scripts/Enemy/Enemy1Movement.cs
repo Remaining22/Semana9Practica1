c41d6beec3ef53244722b9ca09a9cde32dee5dff
Mateo Vallejos <alumnotlsch@campustls.local>
2023-10-23 16:28:11 -0500

    Enemy 1 Movement

---
+++ Assets/Scripts/Enemy/Enemy1Movement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1Movement : MonoBehaviour
+{
+    int Direction = 1;
+    private float Timer;
+    [SerializeField] float FlipTime;
+    Rigidbody2D rb2d;
+    [SerializeField] int movespeed;
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void ChangeDirection()
+    {
+        if (Timer >= FlipTime )
+        {
+            Direction *= -1;
+            Timer = 0;
+        }    
+    }
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        Move();
+        ChangeDirection();
+    }
+    void Move()
+    {
+        rb2d.velocity = new Vector2(Direction, 0) * movespeed;
+    }
+}
